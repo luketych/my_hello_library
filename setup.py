2e2c30f05c90433a33a9530ef5a31f1ee02cf00f
luketych <luke.tych@gmail.com>
2024-01-30 21:45:34 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="my_hello_library",
+    version="0.1.0",
+    description="A simple Python library that provides a hello_world function.",
+    author="Your Name",
+    author_email="your@email.com",
+    py_modules=["my_hello_library"],  # Use py_modules for a single module
+)
