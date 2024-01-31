5f830b88fc34ef679639f4a920cd1a043fa2fac5
luketych <luke.tych@gmail.com>
2024-01-31 15:51:24 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name="my_hello_library",
-    version="0.1.0",
+    version="0.2.0",
     description="A simple Python library that provides a hello_world function.",
     author="Your Name",
     author_email="your@email.com",
+    license='MIT',
    # py_modules=["my_hello_library"],  # Use py_modules for a single module
     url='https://github.com/luketych/my_hello_library',
     packages=['my_hello_library'],
-    scripts=['bin/test.py'],
+    # scripts=['bin/test.py'],
     zip_safe=False
 )
