4cf9c88b43ac04905ac2b8a9ca80af9cb413e53f
luketych <luke.tych@gmail.com>
2024-01-31 15:46:29 -0800

    4th

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ setup(
     description="A simple Python library that provides a hello_world function.",
     author="Your Name",
     author_email="your@email.com",
-    py_modules=["my_hello_library"],  # Use py_modules for a single module
+   # py_modules=["my_hello_library"],  # Use py_modules for a single module
+    packages=['my_hello_library'],
+    scripts=['bin/test.py'],
+    zip_safe=False
 )
